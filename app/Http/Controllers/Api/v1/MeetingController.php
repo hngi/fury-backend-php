73c4da934580fa40c6f3f9eab86fa58c484ff083
ekpono <ekponoambrose@gmail.com>
2020-06-12 14:58:48 +0100

    Fetch all meetings

--- app/Http/Controllers/Api/v1/MeetingController.php
+++ app/Http/Controllers/Api/v1/MeetingController.php
@@ -8,8 +8,25 @@ use Illuminate\Http\Request;
 
 class MeetingController extends Controller
 {
+    public function index()
+    {
+        try{
+            $meeting = Meeting::paginate(12);
+
+            return response()->json([
+                'data' => $meeting,
+                'message' => 'Meeting successfully fetch'
+            ]);
+        }catch (\Exception $e) {
+            return response()->json([
+                'data' => null,
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
     public function create(Request $request)
     {
+
         try {
             $validatedData = $request->validate([
                 'event_id' => 'required|integer|max:255',
